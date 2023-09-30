49404313b48657dd0223e019287c5703364540ee
abhishek khandelwal <abhishekkhandelwal1212@gmail.com>
2023-10-01 01:35:14 +0530

    express server setup

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from "express"
+import express from "express";
 
 const app = express();
 
-app.get("/" , (req , res)=> {
-    res.send("Hello Welcome to auth service")
-})
+app.get("/", (req, res) => {
+  res.send("Hello Welcome to auth service");
+});
 export default app;
