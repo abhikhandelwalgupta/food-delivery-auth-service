3c074b7666fa884f23babb88017868b6ab6f36ec
abhikhandelwalgupta <abhishekkhandelwal1212@gmail.com>
2023-12-25 22:26:52 +0530

    write test

--- tests/users/register.test.ts
+++ tests/users/register.test.ts
@@ -2,32 +2,32 @@ import request from "supertest";
 import app from "../../src/app";
 
 describe("POST", () => {
-    describe("Give all fields", () => {
-        it("Should return the 201 status code ", async () => {
-            const userData = {
-                firstName: "Abhishek",
-                lastName: "Khandelwal",
-                email: "abhishekkhandelwal1212@gmail.com",
-                passowrd: "secret",
-            };
-            const response = await request(app).post("/auth/register").send(userData);
-            expect(response.statusCode).toBe(201);
-        });
-
-        it("should return valid json response ", async () => {
-            const userData = {
-                firstName: "Abhishek",
-                lastName: "Khandelwal",
-                email: "abhishekkhandelwal1212@gmail.com",
-                passowrd: "secret",
-            };
-            const response = await request(app).post("/auth/register").send(userData);
-
-            expect((response.headers as Record<string, string>)['content-type']).toEqual(expect.stringContaining("json"))
-        });
-
-        it("should persist the user in the database ", () => {
-
-        })
+  describe("Give all fields", () => {
+    it("Should return the 201 status code ", async () => {
+      const userData = {
+        firstName: "Abhishek",
+        lastName: "Khandelwal",
+        email: "abhishekkhandelwal1212@gmail.com",
+        passowrd: "secret",
+      };
+      const response = await request(app).post("/auth/register").send(userData);
+      expect(response.statusCode).toBe(201);
     });
+
+    it("should return valid json response ", async () => {
+      const userData = {
+        firstName: "Abhishek",
+        lastName: "Khandelwal",
+        email: "abhishekkhandelwal1212@gmail.com",
+        passowrd: "secret",
+      };
+      const response = await request(app).post("/auth/register").send(userData);
+
+      expect(
+        (response.headers as Record<string, string>)["content-type"],
+      ).toEqual(expect.stringContaining("json"));
+    });
+
+    it("should persist the user in the database ", () => {});
+  });
 });
