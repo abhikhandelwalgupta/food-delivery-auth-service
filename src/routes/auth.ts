c07b04470e7ce7f39e6fafd9e871064d00edcadd
abhikhandelwalgupta <abhishekkhandelwal1212@gmail.com>
2023-12-27 00:17:20 +0530

    add test for persist user

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import express from "express";
 import { AuthController } from "../controllter/AuthController";
+import { UserService } from "../services/UserService";
+import { AppDataSource } from "../config/data-source";
+import { User } from "../entity/User";
 
 const router = express.Router();
-const authControlloer = new AuthController();
+
+const userRepository = AppDataSource.getRepository(User)
+const userService = new UserService(userRepository)
+const authControlloer = new AuthController(userService);
 
 router.post("/register", (req, res) => authControlloer.register(req, res));
 
--- tests/users/register.test.ts
+++ tests/users/register.test.ts
@@ -1,25 +1,48 @@
 import request from "supertest";
 import app from "../../src/app";
+import { AppDataSource } from "../../src/config/data-source";
+import { DataSource } from "typeorm";
+import { truncateTables } from "../utils";
+import { User } from "../../src/entity/User";
 
-describe("POST", () => {
+
+describe("POST /auth/register", () => {
+
+  let connection: DataSource
+
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize()
+  })
+
+  beforeEach(async () => {
+    await truncateTables(connection)
+  })
+
+  afterAll(async () => {
+    await connection.destroy()
+  })
+
+
   describe("Give all fields", () => {
     it("Should return the 201 status code ", async () => {
       const userData = {
         firstName: "Abhishek",
         lastName: "Khandelwal",
         email: "abhishekkhandelwal1212@gmail.com",
-        passowrd: "secret",
+        password: "secret",
       };
       const response = await request(app).post("/auth/register").send(userData);
       expect(response.statusCode).toBe(201);
     });
 
     it("should return valid json response ", async () => {
+
+
       const userData = {
         firstName: "Abhishek",
         lastName: "Khandelwal",
         email: "abhishekkhandelwal1212@gmail.com",
-        passowrd: "secret",
+        password: "secret",
       };
       const response = await request(app).post("/auth/register").send(userData);
 
@@ -28,6 +51,24 @@ describe("POST", () => {
       ).toEqual(expect.stringContaining("json"));
     });
 
-    it("should persist the user in the database ", () => {});
+    it("should persist the user in the database ", async () => {
+      jest.setTimeout(10000)
+      const userData = {
+        firstName: "Abhishek",
+        lastName: "Khandelwal",
+        email: "abhishekkhandelwal1212@gmail.com",
+        password: "abhishek",
+      };
+
+      await request(app).post("/auth/register").send(userData)
+      const userRepository = connection.getRepository(User)
+      const users = await userRepository.find()
+
+      expect(users).toHaveLength(1)
+      expect(users[0].firstName).toBe(userData.firstName)
+      expect(users[0].lastName).toBe(userData.lastName)
+      expect(users[0].email).toBe(userData.email)
+
+    });
   });
 });
