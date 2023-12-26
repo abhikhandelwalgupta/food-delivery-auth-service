740a35f5d0fff583f896f6ffe2ee59fc4291ad4b
abhikhandelwalgupta <abhishekkhandelwal1212@gmail.com>
2023-12-26 21:53:14 +0530

    setup database config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,10 +1,17 @@
 import { config } from "dotenv";
+import path from "path"
 
-config();
 
-const { PORT, NODE_ENV } = process.env;
+config({ path: path.join(__dirname, `../../.env.${process.env.NODE_ENV}`) });
+
+const { PORT, NODE_ENV, DB_USERNAME, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME } = process.env;
 
 export const Config = {
   PORT,
   NODE_ENV,
+  DB_USERNAME,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME
 };
