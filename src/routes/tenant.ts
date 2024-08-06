c062485e2abfeea1dbff3411b544a75d9a2a0744
Abhishek khandelwal <abhishekkhandelwal1212@gmail.com>
2024-08-06 15:36:12 +0530

    check user is authentiate for create tenants

--- src/routes/tenant.ts
+++ src/routes/tenant.ts
@@ -5,6 +5,7 @@ import { TenantService } from "../services/TenantService";
 import { AppDataSource } from "../config/data-source";
 import { Tenant } from "../entity/Tenant";
 import logger from "../config/logger";
+import authenticate from "../middlewares/authenticate";
 
 const router = express.Router();
 
@@ -12,5 +13,5 @@ const router = express.Router();
 const tenantRepository = AppDataSource.getRepository(Tenant);
 const tenantService = new TenantService(tenantRepository);
 const tenantController = new TenantController(tenantService, logger);
-router.post("/", (req, res, next) => tenantController.create(req, res, next));
+router.post("/",authenticate ,(req, res, next) => tenantController.create(req, res, next));
 export default router;
--- tests/tenants/create.test.ts
+++ tests/tenants/create.test.ts
@@ -1,6 +1,3 @@
-/* eslint-disable no-console */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-
 import createJWKSMock from "mock-jwks";
 import { AppDataSource } from "../../src/config/data-source";
 import { DataSource } from "typeorm";
@@ -8,41 +5,67 @@ import request from "supertest";
 import app from "../../src/app";
 import { Tenant } from "../../src/entity/Tenant";
 // import { Tenant } from "../../src/entity/Tenant";
-// import { Roles } from "../../src/constants";
+import { Roles } from "../../src/constants";
 
 describe("POST /tenants", () => {
-  describe("POST /tenants", () => {
-    let connection: DataSource;
+  let connection: DataSource;
+  let jwks: ReturnType<typeof createJWKSMock>;
+  let adminToken: string;
 
-    beforeAll(async () => {
-      connection = await AppDataSource.initialize();
+  beforeAll(async () => {
+    connection = await AppDataSource.initialize();
+    jwks = createJWKSMock("http://localhost:5501");
+  });
+
+  beforeEach(async () => {
+    await connection.dropDatabase();
+    await connection.synchronize();
+    jwks.start();
+    adminToken = jwks.token({
+      sub: "1",
+      role: Roles.ADMIN,
+    });
+    // await truncateTables(connection);
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
+  afterEach(() => {
+    jwks.stop();
+  })
+
+  describe("Give all fields", () => {
+    it("should return a 401 status code", async () => {
+      const tenantData = {
+        name: "Tenant name",
+        address: "Jaipur",
+      };
+      const response = await request(app).post("/tenants").send(tenantData);
+      expect(response.statusCode).toBe(401);
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
+
+      expect(tenants).toHaveLength(0);
     });
 
-    beforeEach(async () => {
-      // await connection.dropDatabase();
-      await connection.synchronize();
-      // await truncateTables(connection);
-    });
+    it("should create a tenant in the database", async () => {
+      const tenantData = {
+        name: "Tenant name",
+        address: "Jaipur",
+      };
+      await request(app)
+        .post("/tenants")
+        .set("Cookie", [`accessToken=${adminToken}`])
+        .send(tenantData);
 
-    afterAll(async () => {
-      await connection.destroy();
-    });
+      const tenantRepository = connection.getRepository(Tenant);
+      const tenants = await tenantRepository.find();
 
-    describe("Give all fields", () => {
-      it("should return a 201 status code", async () => {
-        const tenantData = {
-          name: "Tenant name",
-          address: "Jaipur",
-        };
-        await request(app).post("/tenants").send(tenantData);
-
-        const tenantRepository = connection.getRepository(Tenant);
-        const tenants = await tenantRepository.find();
-
-        expect(tenants).toHaveLength(1);
-        expect(tenants[0].name).toBe(tenantData.name);
-        expect(tenants[0].address).toBe(tenantData.address);
-      });
+      expect(tenants).toHaveLength(1);
+      expect(tenants[0].name).toBe(tenantData.name);
+      expect(tenants[0].address).toBe(tenantData.address);
     });
   });
 });
